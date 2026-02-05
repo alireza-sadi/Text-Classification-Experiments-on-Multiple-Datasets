b10173d220c6bec47a0a2977ae341bc95f63eb73
Alireza Sadi <116587064+alireza-sadi@users.noreply.github.com>
2026-02-05 18:51:52 +0330

    Create 6-DecisionTree.py

---
+++ 6-DecisionTree.py
@@ -0,0 +1,72 @@
+!pip install -q optuna scikit-learn
+!pip install -U datasets fsspec
+
+from datasets import load_dataset
+
+dataset = load_dataset("imdb")
+train_texts = dataset['train']['text']
+train_labels = dataset['train']['label']
+test_texts = dataset['test']['text']
+test_labels = dataset['test']['label']
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(
+    max_features=10000,
+    stop_words='english',
+    ngram_range=(1, 2)
+)
+
+X_train = vectorizer.fit_transform(train_texts)
+X_test = vectorizer.transform(test_texts)
+y_train = train_labels
+y_test = test_labels
+
+
+import optuna
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+
+def objective(trial):
+    params = {
+        'max_depth': trial.suggest_int('max_depth', 3, 30),
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 10),
+        'criterion': trial.suggest_categorical('criterion', ['gini', 'entropy']),
+        'random_state': 42
+    }
+
+    model = DecisionTreeClassifier(**params)
+    scores = cross_val_score(model, X_train, y_train, cv=3, scoring='f1')
+    return scores.mean()
+
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=20)
+
+print("Best hyperparameters:", study.best_params)
+print("Best F1 in Cross-Validation:", study.best_value)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+# ساخت مدل نهایی با بهترین پارامترها
+best_params = study.best_params
+final_model = DecisionTreeClassifier(**best_params)
+final_model.fit(X_train, y_train)
+y_pred = final_model.predict(X_test)
+
+# ارزیابی مدل
+print("🎯 Results of  Decision Tree:")
+print(f"Accuracy :  {accuracy_score(y_test, y_pred):.4f}")
+print(f"Precision:  {precision_score(y_test, y_pred):.4f}")
+print(f"Recall   :  {recall_score(y_test, y_pred):.4f}")
+print(f"F1 Score :  {f1_score(y_test, y_pred):.4f}")
+print("\n📋 full report :\n", classification_report(y_test, y_pred, digits=4))
+
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(final_model, X_train, y_train, cv=5, scoring='accuracy')
+print("\n📊 Cross-Validation Accuracy in each fold:", scores)
+print("Mean:", scores.mean())
+print("Standard deviation :", scores.std())
+
